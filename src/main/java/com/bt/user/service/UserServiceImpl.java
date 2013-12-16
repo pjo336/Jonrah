b8d2563ce6b11967d40dcd04346ec9d8e746dee4
Peter Johnston <pemjohns@gmail.com>
2013-12-15 23:41:57 -0600

    Added to DBSession (Untested)

--- src/main/java/com/bt/user/service/UserServiceImpl.java
+++ src/main/java/com/bt/user/service/UserServiceImpl.java
@@ -1,14 +1,13 @@
 package com.bt.user.service;
 
-import java.util.List;
-
+import com.bt.user.User;
+import com.bt.user.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.bt.user.User;
-import com.bt.user.dao.UserDao;
+import java.util.List;
 
 @Service
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
@@ -16,6 +15,10 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
     
     public void addUser(User user) {
         userDao.saveUser(user);
