adf6b7e1a000b81227d0d41fb9e1a29df6540281
David Hara <hakuhen@gmail.com>
2013-12-18 19:23:25 -0600

    random clean up

--- src/main/resources/sqlscript.sql
+++ src/main/resources/sqlscript.sql
@@ -1,7 +1,7 @@
 # drop table user if exists
 DROP TABLE IF EXISTS USER;
 # USER TABLE
-create table user (
+create table USER (
 	id bigint(11) not null auto_increment,
 	first_name varchar(45) default null,  
     last_name varchar(45) default null,  
@@ -12,7 +12,7 @@ create table user (
 );
 
 # initial data to enter
-insert into User(first_name, last_name, gender, email, user_type)
+insert into USER(first_name, last_name, gender, email, user_type)
 values("Peter", "Johnston", 0, "pjohnston@biggertime.com", 0);
-insert into User(first_name, last_name, gender, email, user_type)
+insert into USER(first_name, last_name, gender, email, user_type)
 values("David", "Hara", 0, "dhara@biggertime.com", 0);
